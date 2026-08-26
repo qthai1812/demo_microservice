32ed0366107794f7cdd500ab88f3ef808a96ef61
Quoc Thai <thaixuanson372@gmail.com>
2026-08-27 01:32:32 +0700

    fix: resolve GlobalExceptionHandler parameter mismatch in all services

--- notification-service/src/main/java/com/devteria/notification/exception/GlobalExceptionHandler.java
+++ notification-service/src/main/java/com/devteria/notification/exception/GlobalExceptionHandler.java
@@ -19,7 +19,7 @@ public class GlobalExceptionHandler {
     private static final String MIN_ATTRIBUTE = "min";
 
     @ExceptionHandler(value = Exception.class)
-    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
+    ResponseEntity<ApiResponse> handlingException(Exception exception) {
         log.error("Exception: ", exception);
         ApiResponse apiResponse = new ApiResponse();
 
